dc9a317ad24c3cb015907330303ce55305eaa984
Dumilde Silva <domilde.silva14@gmail.com>
2023-12-16 17:10:17 +0100

    Aulas de Rotas

--- Aula09 - Atualizando Lote da Lista.py
+++ Aula09 - Atualizando Lote da Lista.py
@@ -4,7 +4,7 @@ from flet import *
 def main(page: Page):
     lv = ListView(expand=1, spacing=10, item_extent=50)
     page.add(lv)
-    for i in range(5000):
+    for i in range(501):
         lv.controls.append(
             Text(f'Linha {i}')
         )
--- Aula11 - Rotas e Navegação Parte 2.py
+++ Aula11 - Rotas e Navegação Parte 2.py
@@ -6,16 +6,40 @@ def main(page: Page):
 
 
     def nova_rota(route):
-        View(
-            '/',
-            [
-                AppBar(title=Text('Flet App'), bgcolor=colors.SURFACE)
-            ]
+        page.views.clear()
+        page.views.append(
+            View(
+                '/',
+                [
+                    AppBar(title=Text('Flet App'), bgcolor=colors.SURFACE_VARIANT),
+                    ElevatedButton('Visite a Loja', on_click=lambda _: page.go("/loja"))
+                ]
+            )
         )
-    page.update()
+
+        if page.route == "/loja":
+            page.views.append(
+                View(
+                    "/loja",
+                    [
+                        AppBar(title=Text("Loja"), bgcolor=colors.SURFACE_VARIANT),
+                        ElevatedButton("Página Inicial", on_click=lambda _: page.go("/"))
+                    ]
+                )
+            )
+
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
     
     
-    page.on_route_change = nova_rota 
+    page.on_route_change = nova_rota
+    page.on_view_pop = view_pop
+    page.go(page.route)
 
 
 app(target=main, view=WEB_BROWSER)
